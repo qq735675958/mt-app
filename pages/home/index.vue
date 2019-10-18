fff0774dfb85bba7e4eea8707dba7edbf7e12b6a
huyu <qq735675958@qq.com>
2019-10-18 09:05:15 +0800

    mt-app

---
+++ pages/home/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">
+        首页
+      </h1>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import Logo from '~/components/Logo.vue'
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style lang="scss">
+.container {
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  color: red;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+  &:hover{
+    color: red;
+  }
+}
+
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">
+        my-app
+      </h1>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+  }
+}
+</script>
+
+<style>
+
+</style>
